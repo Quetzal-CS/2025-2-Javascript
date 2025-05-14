6a21cb73f126610f20632b35c8f5401c648d1360
Quetzal-CS <xochiquetzatl2@gmail.com>
2025-05-14 16:32:44 -0600

    14-05/2025

--- semana2/clase2_tarea.js
+++ semana2/clase2_tarea.js
@@ -1,10 +1,23 @@
+// 🏠 Práctica para casa: Gestor interactivo de frutas (variante)
+// 🎯 Objetivo:
+// Crear un programa que gestione una lista de frutas con opciones para agregar, eliminar, ver la lista completa y salir. El usuario puede interactuar con el menú cuantas veces quiera hasta decidir salir.
+//
+// 🧩 Instrucciones:
+// Crea un arreglo vacío llamado frutas.
+//
+// Usa un ciclo while que muestre el siguiente menú:
+// ¿Qué deseas hacer?
+// 1. Agregar fruta
+// 2. Eliminar fruta
+// 3. Ver lista de frutas
+// 4. Salir
 const { ask } = require('../helpers/input');
 
 async function main() {
   let opcion = '';
   let frutas = [];
 
-  while (opcion !== '3') {
+  while (opcion !== '4') {
     opcion = await ask(`¿Qué deseas hacer?\\n1. Agregar una fruta\\n2. Eliminar una fruta\\n3. Salir`)
 
     if (opcion === '1') {
@@ -28,7 +41,7 @@ async function main() {
 
         console.log(`Frutas actuales ${frutas}`);
       }
-    } else if (opcion === '3') {
+    } else if (opcion === '4') {
       console.log('Gracias por usar el programa. Tus frutas fueron: ', frutas);
     } else {
       console.log('Opción no permitida. Intenta de nuevo');
