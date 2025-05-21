82c974f1ecafc5119ffd4ec4bca685ab7bfcd191
Quetzal-CS <xochiquetzatl2@gmail.com>
2025-05-20 21:04:22 -0600

    Sem2_clase2_tarea

--- semana2/clase2_tarea.js
+++ semana2/clase2_tarea.js
@@ -11,3 +11,56 @@
 // 2. Eliminar fruta
 // 3. Ver lista de frutas
 // 4. Salir
+const { ask } = require('../helpers/input.js');
+
+async function main() {
+  
+    const frutas = [];
+    let opcion = '';
+    
+    while (opcion !== '4') {
+        console.log('¿Qué deseas hacer?');
+        console.log('1. Agregar fruta');
+        console.log('2. Eliminar fruta');
+        console.log('3. Ver lista de frutas');
+        console.log('4. Salir');
+    
+        opcion = await ask('Selecciona una opción: ');
+    
+        switch (opcion) {
+        case '1':
+            const frutaAgregar = await ask('Ingresa el nombre de la fruta a agregar: ');
+            frutas.push(frutaAgregar);
+            console.log(`Fruta "${frutaAgregar}" agregada.`);
+            break;
+        case '2':
+            const frutaEliminar = await ask('Ingresa el nombre de la fruta a eliminar: ');
+            const index = frutas.indexOf(frutaEliminar);
+            if (index !== -1) {
+            frutas.splice(index, 1);
+            console.log(`Fruta "${frutaEliminar}" eliminada.`);
+            } else {
+            console.log(`Fruta "${frutaEliminar}" no encontrada.`);
+            }
+            break;
+        case '3':
+            console.log('Lista de frutas:');
+            if (frutas.length > 0) {
+            frutas.forEach((fruta, index) => {
+                console.log(`${index + 1}. ${fruta}`);
+            });
+            } else {
+            console.log('No hay frutas en la lista.');
+            }
+            break;
+        case '4':
+            console.log('Saliendo del programa...');
+            break;
+        default:
+            console.log('Opción no válida. Intenta nuevamente.');
+        }
+    }
+
+}
+
+main();
