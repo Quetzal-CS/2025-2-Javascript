7743d511dfe710488b203fe5ac5b954c8c4dfd80
Quetzal-CS <xochiquetzatl2@gmail.com>
2025-05-20 21:00:42 -0600

    Sem2_clase1_tarea

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -2,7 +2,16 @@
 // Imprimir números pares del 1 al 100
 const { ask } = require('../helpers/input');
 
-async function main() {
+async function main() { 
+  const numero = Number(await ask("Ingresa un número:"));
+  let i = 1;
+  while (i <= numero) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+    i++;
+  }
+
 
 }
 
