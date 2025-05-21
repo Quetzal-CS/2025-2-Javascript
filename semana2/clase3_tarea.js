d1e3a2ad8c210a8b5e9d41d108b81d5bb7db32c3
Quetzal-CS <xochiquetzatl2@gmail.com>
2025-05-20 21:05:26 -0600

    Sem2_Clase3_tarea

--- semana2/clase3_tarea.js
+++ semana2/clase3_tarea.js
@@ -9,3 +9,29 @@
 // Cuenta cuántos tienen 18 años o más.
 //
 // Muestra el total.
+const { ask } = require('../helpers/input.js');
+
+async function main() {
+  
+    const edades = [];
+    let edad;
+    
+    do {
+        edad = Number(await ask('Ingresa una edad (o -1 para terminar): '));
+        if (edad !== -1) {
+        edades.push(edad);
+        }
+    } while (edad !== -1);
+    
+    let contador = 0;
+    
+    for (const e of edades) {
+        if (e >= 18) {
+        contador++;
+        }
+    }
+    
+    console.log(`Total de personas que pueden votar: ${contador}`);
+}
+
+main();
